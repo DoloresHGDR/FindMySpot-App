cd73ebd7e68a72289dace5381585a44b9663bd92
Pachi69 <pachisepulveda53@gmail.com>
2025-05-01 22:07:24 -0300

    #4 CRUD plates

---
+++ backend/src/main/java/com/example/backend/models/Plates.java
@@ -0,0 +1,33 @@
+package com.example.backend.models;
+import jakarta.persistence.*;
+
+
+@Entity
+public class Plates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String number;
+
+    public Plates() {
+    }
+
+    public Plates(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+}
+
