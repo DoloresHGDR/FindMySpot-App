13196c7baaed500659263d35d5f38c8aa8ebf021
Pachi69 <pachisepulveda53@gmail.com>
2025-05-13 19:24:39 -0300

    #8 Pantalla de login

--- FindMySpot/app/_layout.tsx
+++ FindMySpot/app/_layout.tsx
@@ -1,11 +1,21 @@
 import { Stack } from "expo-router";
 import { UserProvider } from "@/context/UserContext";
 export default function RootLayout() {
-  return(
-    <UserProvider>
-      <Stack />
-    </UserProvider>
-    
+  return (
+      <UserProvider>
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#fff',
+            },
+            headerTintColor: 'black',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
+          <Stack.Screen name="index" />
+        </Stack>
+      </UserProvider>
   );
   
 }
