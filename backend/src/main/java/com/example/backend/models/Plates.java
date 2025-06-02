ab23567548be04de05af80b8860a4dd1e9d275b7
Pachi69 <pachisepulveda53@gmail.com>
2025-06-02 12:36:44 -0300

    #4 Modificado modelo de patentes

--- backend/src/main/java/com/example/backend/models/Plates.java
+++ backend/src/main/java/com/example/backend/models/Plates.java
@@ -1,65 +1,27 @@
 package com.example.backend.models;
 import jakarta.persistence.*;
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 @Entity
 public class Plates {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String number;
-    private boolean parked;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private Users user;
-    @OneToMany(mappedBy = "plate", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Parkings> parkings;
-    @OneToMany(mappedBy = "plate", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Fines> fines;
-
 
     public Plates() {
     }
 
-    public Plates(String number, boolean parked, Users user, List<Parkings> parkings, List<Fines> fines) {
+    public Plates(String number, Users user) {
         this.number = number;
-        this.parked = parked;
         this.user = user;
-        this.parkings = parkings;
-        this.fines = fines;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNumber() {
-        return number;
-    }
-    public void setNumber(String number) {
-        this.number = number;
-    }
-    public boolean getParked() {
-        return parked;
-    }
-    public void setParked(boolean parked) {
-        this.parked = parked;
-    }
-    public Users getUser() {
-        return user;
-    }
-    public void setUser(Users user) {
-        this.user = user;
-    }
-    public List<Parkings> getParkings() {
-        return parkings;
-    }
-    public void setParkings(List<Parkings> parkings) {
-        this.parkings = parkings;
-    }
-
+    };
 }
 
