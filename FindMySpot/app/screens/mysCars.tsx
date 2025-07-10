707bf81055a4f087b5185e7f4c47927fbf1e8a81
Dolly <dg.herrera@alumno.um.edu.ar>
2025-07-10 11:00:23 -0300

    #17 Screen de myplates

--- FindMySpot/app/screens/mysCars.tsx
+++ FindMySpot/app/screens/mysCars.tsx
@@ -1,10 +1,31 @@
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import HomeButtons from '@/components/buttons'
+import Buttons from '@/components/buttons';
+import PatenteModal from '@/components/plateModal';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const abrirModal = () => setModalVisible(true);
+  const cerrarModal = () => setModalVisible(false);
+
   return (
     <View style={styles.container}>
-      <Text>Autos asociados</Text>
+      <Text style={styles.tittle}>Mis Patentes</Text>
+      
+            <View style={styles.position}>
+        <Buttons
+          tittle="Añadir patente"
+          onPress={() => setModalVisible(true)}
+        />
+      </View>
+
+      <PatenteModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
     </View>
   );
 }
@@ -12,7 +33,23 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 30,
+    backgroundColor: '#1a1a19',
   },
+
+  tittle: {
+    fontFamily: 'arial',
+    fontSize: 25,
+    color: '#43985b',
+    fontWeight: 600,
+  },
+  
+  position: {
+    alignItems: 'center',
+    position: 'static',
+    display: 'flex',
+    flexDirection: 'column',
+    height: 100,
+    marginTop: 'auto'
+  }
 });
