a13829b6fc168b7959a50e00be9432c6c8366f9c
Dolly <dg.herrera@alumno.um.edu.ar>
2025-08-23 23:44:11 -0300

    #19 Layout screen plates

--- FindMySpot/app/_layout.tsx
+++ FindMySpot/app/_layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout() {
             <Stack.Screen name="register" />
             <Stack.Screen name="screens/home" />
             <Stack.Screen name="screens/parking" />
-            <Stack.Screen name="screens/mysCars" />
+            <Stack.Screen name="screens/plates" />
             <Stack.Screen name="screens/history" />
             <Stack.Screen name="screens/parkingDetailsScreen" />
         </Stack>
