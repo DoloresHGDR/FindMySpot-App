ae4485d1d63a76b66ad7723cf09adbb594717910
Dolly <dg.herrera@alumno.um.edu.ar>
2025-09-17 17:36:44 -0300

    #19 Cambio en la vista de plates

--- FindMySpot/app/screens/plates.tsx
+++ FindMySpot/app/screens/plates.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Alert } from 'react-native';
+import { useEffect, useState } from 'react'; 
+import { View, Text, StyleSheet, Alert, ImageBackground } from 'react-native';
+import { useFonts } from 'expo-font';
+import AwesomeAlert from 'react-native-awesome-alerts';
 import Buttons from '@/components/buttons';
 import PatenteModal from '@/components/plateModal';
 import { useUser } from '@/context/UserContext';
@@ -7,8 +9,10 @@ import { AxiosError } from 'axios';
 import apiClient from '@/api/apiClient';
 
 export default function myPlatesScreen() {
+  const [showAlert, setShowAlert] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
-  const [plates, setPlates] = useState<string[]>([]);
+  const [fontsLoaded] = useFonts({'FE-Font': require('@/assets/fonts/FE-FONT.ttf')});
+  const [plates, setPlates] = useState([]);
   const { user } = useUser();
 
   const abrirModal = () => setModalVisible(true);
@@ -17,40 +21,124 @@ export default function myPlatesScreen() {
   const fetchPlates = async () => {
     try {
       const response = await apiClient.get(`/api/plates/user/${user.id}`);
-      const plateNumbers = response.data.map((plate: any) => plate.number);
+      const plateNumbers = response.data;
       setPlates(plateNumbers);
     } catch (error) {
       const err = error as AxiosError;
       if (err.response) {
-        Alert.alert('Error', err.response.data as string || "Error al obtener plates");
+        Alert.alert('Error', (err.response.data as string) || "Error al obtener plates");
       } else {
         Alert.alert('Error', "No se pudo conectar con el servidor.");
       }
     }
   };
 
+  const handleDelete = async (plate: any) => {
+    console.log(plates)
+    setShowAlert(false);
+    try {
+      await apiClient.delete(`/api/plates/${plate}`);
+      setPlates(prev => prev.filter(p => p !== plate));
+    } catch (error) {
+      Alert.alert("Error", "No se pudo eliminar la patente.");
+    }
+  };
+
   useEffect(() => {
     fetchPlates();
-  }, []);
+  }, [plates]);
+
+  const isNewFormat = (plate: any) => {
+    return /^[A-Z]{2}\s?[0-9]{3}\s?[A-Z]{2}$/.test(plate); 
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.tittle}>Mis Patentes</Text>
 
-      {/* Lista de placas */}
       {plates.length > 0 ? (
-        <View style={{ marginTop: 20 }}>
-          {plates.map((plate, index) => (
-            <Text
-              key={index}
-              style={{ color: '#ebebeb', fontSize: 18, marginBottom: 5 }}
-            >
-              • {plate}
-            </Text>
-          ))}
+        <View style={styles.platesContainer}>
+          {plates.map((plate, index) => {
+            const imageSource = isNewFormat(plate.number)
+              ? require('@/assets/images/plate_new.png') 
+              : require('@/assets/images/plate_old.png');
+
+            return (
+              <View key={index} style={styles.plateWrapper}>
+                <ImageBackground
+                  source={imageSource}
+                  resizeMode="stretch"
+                  style={styles.plateImage}
+                >
+                  <Text
+                    style={[
+                      styles.plateNumber,
+                      isNewFormat(plate.number) ? styles.newPlateText : styles.oldPlateText
+                    ]}
+                  >
+                    {plate.number}
+                  </Text>
+                </ImageBackground>
+                <Text style={styles.deleteButton} onPress={() => setShowAlert(true)}>
+                  ❌
+                </Text>
+
+                  <AwesomeAlert
+                    show={showAlert}
+                    showProgress={false}
+                    title="Eliminar matricula"
+                    message="¿Seguro que quieres eliminar esta matricula?"
+                    closeOnTouchOutside={true}
+                    closeOnHardwareBackPress={true}
+                    showCancelButton={true}
+                    showConfirmButton={true}
+                    cancelText="Cancelar"
+                    confirmText="Sí"
+                    confirmButtonColor="#43975a"
+                    cancelButtonColor='#974343ff'
+                    onCancelPressed={() => setShowAlert(false)}
+                    onConfirmPressed={() => handleDelete(plate.id)}
+
+                    overlayStyle={{
+                      backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                    }}
+                    confirmButtonStyle={{
+                      paddingVertical: 12,
+                      paddingHorizontal: 40,
+                      borderRadius: 8,    
+                      fontSize: 18,   
+                    }}
+                    cancelButtonStyle={{
+                      paddingVertical: 12,
+                      paddingHorizontal: 20,
+                      borderRadius: 8,
+                      fontSize: 18,
+                    }}
+
+                    titleStyle={{
+                      color: '#e6e6e6',
+                      fontWeight: 'bold'
+                    }}
+
+                    messageStyle={{
+                      color: '#e6e6e6',
+                      textAlign: 'center',
+                      lineHeight: 20,
+                      fontSize: 16
+                    }}
+
+                    contentContainerStyle={{
+                      backgroundColor: '#1a1a19'
+                      
+                    }}
+                  />
+
+              </View>
+            );
+          })}
         </View>
       ) : (
-        <Text style={{ color: '#999', marginTop: 20 }}>Añadi tu plate wachin</Text>
+        <Text style={{ color: '#999', marginTop: 20 }}>Añade tu Matricula</Text>
       )}
 
       <View style={styles.position}>
@@ -64,6 +152,7 @@ export default function myPlatesScreen() {
         visible={modalVisible}
         onClose={cerrarModal}
       />
+
     </View>
   );
 }
@@ -80,9 +169,54 @@ const styles = StyleSheet.create({
     fontSize: 25,
     marginTop: 35, 
     color: '#43985b',
-    fontWeight: 900,
+    fontWeight: '900',
   },
-  
+
+  platesContainer: {
+    marginTop: 20,
+    flexDirection: 'column',
+    gap: 25,
+  },
+
+  plateWrapper: {
+    position: 'relative',
+    width: 280,
+    height: 90,
+    alignSelf: 'center',
+  },
+
+  plateImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100
+  },
+
+  plateNumber: {
+    fontFamily: 'FE-Font', 
+    fontSize: 45,
+    paddingTop: 30
+  },
+
+  oldPlateText: {
+    color: '#fff', 
+    fontSize: 50,
+    paddingTop: 15
+  },
+
+  newPlateText: {
+    color: '#000', 
+    fontSize: 47
+  },
+
+  deleteButton: {
+    position: 'absolute',
+    top: -10,
+    right: -10,
+    fontSize: 22,
+    color: 'red',
+  },
+
   position: {
     alignItems: 'center',
     position: 'static',
