da0298e23fd317dc06a08afc00659e32d4b083bd
Pachi69 <pachisepulveda53@gmail.com>
2025-09-17 18:16:17 -0300

    cambios

--- backend/src/main/java/com/example/backend/repository/UsersRepository.java
+++ backend/src/main/java/com/example/backend/repository/UsersRepository.java
@@ -1,10 +1,12 @@
 package com.example.backend.repository;
 import com.example.backend.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    @Query("SELECT DISTINCT u FROM Users u WHERE u.identityNumber = :identityNumber")
     Users findByIdentityNumber(String identityNumber);
 }
